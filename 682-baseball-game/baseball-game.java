f6f0111ddffcdd4830d266e0dfc497771799b442
sidhkataria222-hub <sidhkataria222@gmail.com>
2026-09-24 01:37:26 +0530

    Time: 3 ms (78.97%) | Memory: 43.2 MB (91.25%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int calPoints(String[] operations) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : operations) {
+
+            if (op.equals("C")) {
+                stack.pop();
+            }
+
+            else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            }
+
+            else if (op.equals("+")) {
+                int last = stack.pop();
+                int secondLast = stack.peek();
+
+                int sum = last + secondLast;
+
+                stack.push(last);
+                stack.push(sum);
+            }
+
+            else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int total = 0;
+
+        while (!stack.isEmpty()) {
+            total += stack.pop();
+        }
+
+        return total;
+    }
+}
